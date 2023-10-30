60119b24e63a8749ed3f32d8dea3ead472340e82
rownygosc <251250@edu.p.lodz.pl>
2023-10-30 12:08:20 +0100

    02 funkcja 'mnozenie'

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
+int iloczyn(int a, int b)
+{
+    return a*b;
+}
 int main() {
     printf("Hello, World!\n");
+    printf("W drugim branchu: 2*3 = ", iloczyn(2,3));
     return 0;
 }
