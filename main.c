507b62bde48b79647a3aa6112b3ea6f5bc805ff8
rownygosc <251250@edu.p.lodz.pl>
2023-10-30 12:05:03 +0100

    01 funkcja 'dodawanie'

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
+int dodawanie(int a, int b)
+{
+    return a+b;
+}
 int main() {
     printf("Hello, World!\n");
+    printf("W pierwszym branchu: 2+3 = ", dodawanie(2,3));
     return 0;
 }
